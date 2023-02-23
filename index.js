63750d3f5ce103094ef87952421a1081b4f1ffd1
Christopher Dean Canlas <chrisdcanlas@gmail.com>
2023-02-23 11:16:48 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(res => res.json())
+  .then(data  => {
+    return renderBooks(data);
+  })
+
 }
 
 function renderBooks(books) {
